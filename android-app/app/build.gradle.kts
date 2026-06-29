2a42d9fc3646506004b7facf8300d83d8f001e24
Valentin <valentin@yccom.ru>
2026-06-29 11:48:42 +0100

    fix: Chaquopy via buildscript classpath + apply

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -1,7 +1,19 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://chaquo.com/maven") }
+    }
+    dependencies {
+        classpath("com.chaquo.python:gradle:15.0.1")
+    }
+}
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 apply(plugin = "com.chaquo.python")
 
 android {
